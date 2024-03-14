451eb53a647d6ed3fdb671f684596bdc0e07baba
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-03-14 11:12:20 +0100

    fizzbuzz.test

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -7,9 +7,19 @@ describe('FizzBuzz', () => {
     it('should return 2', () => {
         expect(fizzbuzz(2)).toBe(2)
     })
-    it('should return "Fizz" for multiples of 3', () => {
+    it('should return "Fizz" for 3', () => {
         expect(fizzbuzz(3)).toBe('Fizz')
+    })
+    it('should return "Fizz" for multiple of 3', () => {
         expect(fizzbuzz(6)).toBe('Fizz')
-        expect(fizzbuzz(9)).toBe('Fizz')
+    })
+    it('should return "Buzz" for 5', () => {
+        expect(fizzbuzz(5)).toBe('Buzz')
+    })
+    it('should return "Buzz" for multiple of 5', () => {
+        expect(fizzbuzz(10)).toBe('Buzz')
+    })
+    it('should return "FizzBuzz" for 15', () => {
+        expect(fizzbuzz(15)).toBe('FizzBuzz')
     })
 })
