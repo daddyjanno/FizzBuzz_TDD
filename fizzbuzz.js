c14bcb42b80de8fb57be3c640168d8aa58dbfa21
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-03-14 11:16:03 +0100

    refacto

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,6 @@
 export function fizzbuzz(number) {
-    if (number % 3 === 0 && number % 5 === 0) {
-        return 'FizzBuzz'
-    } else if (number % 3 === 0) {
-        return 'Fizz'
-    } else if (number % 5 === 0) {
-        return 'Buzz'
-    } else return number
+    if (number % 3 === 0 && number % 5 === 0) return 'FizzBuzz'
+    if (number % 3 === 0) return 'Fizz'
+    if (number % 5 === 0) return 'Buzz'
+    return number
 }
