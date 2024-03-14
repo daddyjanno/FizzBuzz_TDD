f0b3e59655f30bd9f9bbda935856d0b7c722d37f
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2024-03-14 11:12:30 +0100

    fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,6 +1,9 @@
 export function fizzbuzz(number) {
-    if (number % 3 === 0) {
+    if (number % 3 === 0 && number % 5 === 0) {
+        return 'FizzBuzz'
+    } else if (number % 3 === 0) {
         return 'Fizz'
-    }
-    return number
+    } else if (number % 5 === 0) {
+        return 'Buzz'
+    } else return number
 }
